346504e7609c932fbbd334477124901a1e08042a
lovefriendlove2222-collab <lovefriendlove2222@gmail.com>
2026-04-16 17:17:52 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,12 @@ import 'screens/login_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(VikasPlatform());
+  runApp(VikasPlatform());import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart'; 
+import 'screens/login_screen.dart';
+import 'screens/admin_dashboard.dart';
+
 }
 
 class VikasPlatform extends StatelessWidget {

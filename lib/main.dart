90d37f35145d9aba35cd6190e823a737049a8d9e
lovefriendlove2222-collab <lovefriendlove2222@gmail.com>
2026-04-16 17:35:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'screens/login_screen.dart';
 import 'screens/admin_dashboard.dart';
@@ -8,20 +8,46 @@ import 'screens/admin_dashboard.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(VikasPlatform());
+  runApp(VikasApp());
 }
 
-class VikasPlatform extends StatelessWidget {
+class VikasApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Vikas Pasoria Official',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: LoginScreen(), // शुरुआती पेज लॉगिन होगा
+      title: 'Vikas Pasoria',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: AuthWrapper(),
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return FutureBuilder<DocumentSnapshot>(
+            future: FirebaseFirestore.instance.collection('users').doc(snapshot.data!.uid).get(),
+            builder: (context, userSnapshot) {
+              if (userSnapshot.connectionState == ConnectionState.waiting) {
+                return Scaffold(body: Center(child: CircularProgressIndicator()));
+              }
+              if (userSnapshot.hasData && userSnapshot.data!.exists) {
+                var data = userSnapshot.data!.data() as Map<String, dynamic>;
+                if (data['role'] == 'admin') {
+                  return AdminDashboard();
+                }
+              }
+              return Scaffold(body: Center(child: Text("Welcome Member!"))); 
+            },
+          );
+        }
+        return LoginScreen();
+      },
     );
   }
 }

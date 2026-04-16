64b8c597abcceface085083bd39382561e0e3504
lovefriendlove2222-collab <lovefriendlove2222@gmail.com>
2026-04-16 18:06:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-      appBar: AppBar(
-        title: Text("Vikas Pasoria App"),
-        backgroundColor: Colors.red,
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.stars, size: 80, color: Colors.orange),
-            SizedBox(height: 20),
-            Text(
-              "राम-राम विकास भाई!", 
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)
-            ),
-            Text("आपका ऐप अब तैयार हो रहा है..."),
-          ],
+        child: Text(
+          "नमस्ते विकास भाई!\nआपका एप्प तैयार हो रहा है।",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
         ),
       ),
     ),
